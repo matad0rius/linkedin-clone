6f90661c955657b6ea7f2f869072cc8856e6b123
ramon godas lopez <53528392+matad0rius@users.noreply.github.com>
2021-04-02 19:37:59 -0600

    integrating redux

--- src/App.js
+++ src/App.js
@@ -1,20 +1,26 @@
-import './App.css';
-import Header from './Header';
-import Sidebar from './Sidebar';
-import Feed from './Feed';
+import "./App.css";
+import Header from "./Header";
+import Sidebar from "./Sidebar";
+import Feed from "./Feed";
+import { useSelector } from "react-redux";
+import { selectUser } from "./features/userSlice";
+import Login from './Login';
 
 function App() {
+  const user = useSelector(selectUser);
+
   return (
     <div className="App">
       <Header />
 
-      {/* App Body */}
-      <div className="app__body">
-        <Sidebar />
-        <Feed />
-        {/* Widget */}
-      </div>
-
+      {!user ? (
+        <Login />
+      ) : (
+        <div className="app__body">
+          <Sidebar />
+          <Feed />
+        </div>
+      )}
     </div>
   );
 }
